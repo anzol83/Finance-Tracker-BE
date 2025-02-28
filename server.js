ab8c5ebed84da94bff486b8f2bb837b8554cbcef
Angel Neupane <angelneupane83@gmail.com>
2025-03-01 09:06:15 +1100

    post added

--- server.js
+++ server.js
@@ -1,5 +1,30 @@
-// Create express app
-// Run the server
-// Connect with Db
-// Create user route
-// Create user create endpoint | POST
+import "dotenv/config";
+import express from "express";
+import userRouter from "./src/router/userRouter.js";
+import { connectToMongoDB } from "./src/config/dbConfig.js";
+import cors from "cors";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+cors;
+
+connectToMongoDB();
+
+app.use(express.json());
+
+const corsOption = {
+  credential: true,
+  origin: true,
+};
+
+app.use(cors(corsOption));
+
+app.use("/api/users", userRouter);
+
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error.message)
+    : console.log(`Server Running Successfully`);
+});
