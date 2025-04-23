24a10a89fac5738872589ba3bbc3612e13740fc2
Angel Neupane <angelneupane83@gmail.com>
2025-04-23 20:26:22 +1000

    utility edited

--- utils/bcrypt.js
+++ utils/bcrypt.js
@@ -1,5 +1,4 @@
-import bcrypt from "bcryptjs"
-
+import bcrypt from "bcryptjs";
 const saltRound = 10;
 export const hashPassword = (plainPassword) => {
   return bcrypt.hashSync(plainPassword, saltRound);
